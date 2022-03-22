c5dad0506a7b33c93ad6b080e624c3ffc743b27c
jmfs1 <luizcarlosvilela@outlook.com.br>
2022-03-22 19:15:09 -0300

    - Atualizanod https do back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,4 @@
 import axios from "axios";
-// import { getToken } from "./auth";
-
-// const api = axios.create({
-//   baseURL: "https://api2.zerotime.com.br",
-//   // headers: {
-//   //   'Content-Type': 'application/x-www-form-urlencoded'
-//   // }
-// });
-
-// api.interceptors.request.use(async config => {
-//   const token = getToken();
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return config;
-// });
-
-// export default api;
-
-// import { create } from 'apisauce';
-
 export const api = axios.create({
-  baseURL: "http://colidiu.luizcarlosvilela.com.br",
+  baseURL: "https://colidiu.luizcarlosvilela.com.br",
 });
